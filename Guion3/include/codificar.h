1c89fe0b3b62806dd978de5d6fb75af2b95c6be2
Bruno-sm <bru.1bruno@gmail.com>
2015-03-23 12:48:45 +0100

    añadida documentacion codificar

--- Guion3/include/codificar.h
+++ Guion3/include/codificar.h
@@ -1,7 +1,34 @@
+/**
+  * @file codificar.h
+  * @brief Fichero cabecera para la codificacion de mensajes en imágenes
+  *
+  * Permite la codificacion de mensajes en mapas de bits
+  *
+  */
+
 #ifndef _CODIFICAR_H_
 #define _CODIFICAR_H_
 
+
+/**
+  * @brief Oculta un mensaje en un mapa de bits
+  *
+  * @param mapa de bits que conforma la imagen.
+  * @param longitud del mapa de bits en forma de vector.
+  * @param cadena c con el mensaje a ocultar.
+  * @return Devuelve si se ha ocultado con exito
+  * @pre la imagen debe tener una longitud ocho veces mayor al mensaje.
+  */
 bool Ocultar(unsigned char imagen[], int longitud, char msg[]);
+
+/**
+  * @brief Revela un mensaje oculto en un mapa de bits
+  *
+  * @param mapa de bits que conforma la imagen.
+  * @param cadena c en la que se almacena el mensaje oculto.
+  * @param longitud maxima que se llegara a leer.
+  * @return Devuelve si se ha revelado con exito
+  */
 bool Revelar(unsigned char imagen[], char msg[], int n);
 
 #endif
