3d72572a93b1743ec25f17f886a9069fb92c53fc
Igunashio-sama <ignaciobarraganlozano@yahoo.es>
2015-04-10 17:50:39 +0200

    Implementaciones 3 y 4 corregidas

--- Guion4/src/matriz_bit3.cpp
+++ Guion4/src/matriz_bit3.cpp
@@ -17,7 +17,7 @@ int Filas (const MatrizBit& m){
 	return m.dimension >> 16;
 }
 int Columnas(const MatrizBit& m){
-	int columnas = m.dimension & (pow(2.0,16.0) - 1);
+	int columnas = m.dimension & (unsigned int)(pow(2.0,16.0) - 1);
 	return columnas;
 }
 bool Get(const MatrizBit& m, int f, int c){
--- Guion4/src/matriz_bit4.cpp
+++ Guion4/src/matriz_bit4.cpp
@@ -1,7 +1,8 @@
+#include <cmath>
 #include "matriz_bit4.h"
 
 bool Inicializar(MatrizBit& m, int fils, int cols){
-	if (fils * cols >= 128 || fils < 0 || cols < 0)
+	if (fils * cols > 128 || fils < 0 || cols < 0)
 		return false;
 	else{
 		m.dimension = (fils << 8) | cols;
@@ -15,12 +16,12 @@ int Filas (const MatrizBit& m){
 	return m.dimension >> 8;
 }
 int Columnas(const MatrizBit& m){
-	int columnas = m.dimension & (2^8 - 1);
+	int columnas = m.dimension & (unsigned int)(pow(2.0,8.0) - 1);
 	return columnas;
 }
 bool Get(const MatrizBit& m, int f, int c){
 	int columnas = Columnas(m);
-	int posicion = f * (columnas - 1) + c;
+	int posicion = f * columnas + c;
 	int cual_entero = posicion/32;
 	int posicion_bit = posicion - 32*cual_entero;
 	bool valor = (m.matriz[cual_entero] & (1 << posicion_bit)) >> posicion_bit;
@@ -30,7 +31,7 @@ void Set(MatrizBit& m, int f, int c, bool v){
 	int filas = Filas(m);
 	int columnas = Columnas(m);
 	if (f < filas && c < columnas){
-		int posicion = f * (columnas - 1) + c;
+		int posicion = f * columnas + c;
 		int cual_entero = posicion/32;
 		int posicion_bit = posicion - 32*cual_entero;
 		if (v)
