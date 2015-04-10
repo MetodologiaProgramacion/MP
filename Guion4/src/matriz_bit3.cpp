6b5c265a01c78aa4fb698c75c52842c723362d1e
Igunashio-sama <ignaciobarraganlozano@yahoo.es>
2015-04-10 17:25:27 +0200

    Implementacion 3

--- Guion4/src/matriz_bit3.cpp
+++ Guion4/src/matriz_bit3.cpp
@@ -17,7 +17,7 @@ int Filas (const MatrizBit& m){
 	return m.dimension >> 16;
 }
 int Columnas(const MatrizBit& m){
-	int columnas = m.dimension & (2^16 - 1);
+	int columnas = m.dimension & (pow(2.0,16.0) - 1);
 	return columnas;
 }
 bool Get(const MatrizBit& m, int f, int c){
